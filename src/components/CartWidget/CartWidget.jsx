a81f0c5c60bda869a4d4f523375e83f748f3c0b8
Lucas <lucasdavid9509@outlook.com>
2023-04-04 04:56:15 -0300

    Modifico CartWidget

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,12 +1,25 @@
 import React from "react";
+import Badge from "@mui/material/Badge";
+import { styled } from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
 import styles from "./CartWidget.module.css";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
+const StyledBadge = styled(Badge)(({ theme }) => ({
+  "& .MuiBadge-badge": {
+    right: +4,
+    top: -2,
+  },
+}));
+
 const CartWidget = () => {
   return (
-    <div className={styles.cart}>
-      <ShoppingCartIcon />
-      <span className={styles.cartSpan}>0</span>
+    <div className={styles.cartContainer}>
+      <IconButton aria-label="cart">
+        <StyledBadge badgeContent={100} max={99} color="error">
+          <ShoppingCartIcon className={styles.cart} />
+        </StyledBadge>
+      </IconButton>
     </div>
   );
 };
