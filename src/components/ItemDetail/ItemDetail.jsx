b9598d973cb9ecd58a9cefa8f7f1b5641dd61e91
Lucas <lucasdavid9509@outlook.com>
2023-04-24 16:31:04 -0300

    Modifico ItemDetail

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -4,7 +4,11 @@ import styles from "./ItemDetail.module.css";
 const ItemDetail = ({ product }) => {
   return (
     <div className={styles.containerItemdetail}>
-      <img className={styles.itemDetailimg} src={product.img}></img>
+      <img
+        className={styles.itemDetailimg}
+        src={product.img}
+        alt="product detail"
+      ></img>
       <h1 className={styles.itemDetailtitle}>{product.title}</h1>
       <h2 className={styles.itemDetailprice}>{product.price}</h2>
     </div>
